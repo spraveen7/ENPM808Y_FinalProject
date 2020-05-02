2b20ef5c678c3ece464b6320f5361d68f9f7f6f0
Sudharsan Balasubramani <sudharsansci@gmail.com>
2020-05-01 23:38:15 -0400

    DFS Algorithm - search feature added
    - Need to confirm the working
    - Pseudo code of DFS algorithm implemented
    - need to add back tracking once the goal is found!

--- Final_Project/src/DFSAlgorithm/dfsalgorithm.cpp
+++ Final_Project/src/DFSAlgorithm/dfsalgorithm.cpp
@@ -1,58 +1,97 @@
-/**
-* @file dfsalgorithm.cpp
-* @authors Group 4
-*
-* Karan Sutradhar (117037272)
-* Sudharsan Balasubramani (116298636)
-* Sai Bhamidipati (117023640)
-* Ashwin Prabhakaran (117030402)
-* Girish Ethirajan
-*
-* @version 1.0
-*
-* @section LICENSE
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License as
-* published by the Free Software Foundation; either version 2 of
-* the License, or (at your option) any later version.
-*
-* @section DESCRIPTION
-*
-*  This is the cpp for the class dfsalgorithm, implementing the Depth First Search algorithm
-*  in this class and navigating the robot through the maze.
-*/
-
 #include "dfsalgorithm.h"
+#include<iostream>
+#include <vector>
 #include <stack>
 #include <array>
-#include <vector>
-#include <string>
-#include<iostream>
 
-int fp::dfsalgorithm::IsGoal(int x_, int y_) {
-    if((x_ == 7 && y_ == 7) || (x_ == 8 && y_ == 8) || (x_ == 7 && y_ == 8) || (x_ == 8 && y_ == 7))
-        std::cout << "Goal Reached";
-        return 1;
-    return 0;
-}
 
-void fp::dfsalgorithm::Solve(std::shared_ptr<fp::LandBasedRobot>) {
 
-    if(fp::Algorithm::IsGoal(x_, y_)) == 1
-    {
-        while (!stack_y.empty())
+
+namespace fp{
+    struct Node {
+        int x_, y_, dist_;
+        char direction_;
+        std::array<int, 2> parent_node_;
+        Node(){}
+        Node(int x, int y, char direction = (char) " ") : x_(x), y_(y), direction_(direction) {}
+        ~Node(){}
+    };
+
+    class DFSAlgorithm {
+    public:
+        int height, width;
+        char current_direction_;
+        std::stack<std::array <int,2> > stack_;
+        std::array<int, 2> current_node;
+        std::array<std::array<bool, 16>, 16> visited_node_;
+        std::array<std::array<Node, 16>, 16> node_arr_;
+
+
+    public:
+        DFSAlgorithm()
         {
-            fp::api::setColor(stack_y.top(), stack_x.top(), 'Y');
-            stack_y.pop();
-            stack_x.pop();
+            for(int i = 0; i < 16 ; ++i)
+                for (int j = 0; j < 16 ; ++j)
+                {
+                    node_arr_[i][j] = fp::Node{i, j};
+                    visited_node_[i][j] = false;
+                }
         }
-        return;
-    }
-    }
-}
 
-void fp::dfsalgorithm::FindPath(std::shared_ptr<fp::LandBasedRobot> robot, int x_, int y_) {
+        bool IsVisited(std::array<int, 2> cur_node)
+        {
+            return visited_node_.at(cur_node.at(0)).at(cur_node.at(1));
+        }
 
+        void FindNeighbours(std::array<int, 2> cur_node)
+        {
+            std::array<int, 2> node_N, node_W, node_S, node_E;
+
+            /* ---> Neighbour in North <--- */
+            node_N[0] = cur_node[0] - 1;
+            node_N[1] = cur_node[1];
+            if (node_N[0] >= 0)
+                if(!IsVisited(node_N)) stack_.push(node_N);
+
+            /* ---> Neighbour in West <--- */
+            node_W[0] = cur_node[0];
+            node_W[1] = cur_node[1] - 1;
+            if(!IsVisited(node_W)) stack_.push(node_W);
+
+            /* ---> Neighbour in South <--- */
+            node_S[0] = cur_node[0] + 1;
+            node_S[1] = cur_node[1];
+            if (node_N[0] <= 15)
+                if(!IsVisited(node_S)) stack_.push(node_S);
+
+            /* ---> Neighbour in East <--- */
+            node_E[0] = cur_node[0];
+            node_E[1] = cur_node[1] + 1;
+            if(!IsVisited(node_E)) stack_.push(node_E);
+
+        }
+
+        void SearchPath(std::array<int, 2> start, std::array<int, 2> goal)
+        {
+            /* ---> Step 01: Add start to node <--- */
+            stack_.push(start);
+            /* ---> Step 03: loop until stack_ blows <--- */
+            while(!stack_.empty())
+            {
+                /* ---> Step 04: Set Current Node <--- */
+                current_node = stack_.top();
+                stack_.pop();
+                /* ---> Step 05: check IsGoal <--- */
+                if (current_node == goal) stack_.push(current_node);
+                /* ---> Step 06: Check Visited <--- */
+                else if (!IsVisited(current_node)) FindNeighbours(current_node);
+                /* ---> Step 07: Mark visited <--- */
+                visited_node_[current_node[0]][current_node[1]] = true;
+            }
+
+        }
+
+
+    };
 }
 
