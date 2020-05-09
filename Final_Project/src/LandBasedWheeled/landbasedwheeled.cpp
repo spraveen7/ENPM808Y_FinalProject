0acc1edcd9ae2566a4818939178fe99abac8766c
Sudharsan <sudharsansci@gmail.com>
2020-05-08 22:02:00 -0400

    Create LandBasedWheeled Class as per the Final Project requisite PDF
    Features:
        -> Create a derived class LandBasedWheel inheriting LandBasedRobot class
        -> Add Method prototypes and definitions
        -> Add Constructors and Destructors
        -> Create Getters and Setters for all attributes
        -> Add Comments to all the methods, accessors and mutators
    Signed-off-by: Sudharsan <sudharsansci@gmail.com>

--- Final_Project/src/LandBasedWheeled/landbasedwheeled.cpp
+++ Final_Project/src/LandBasedWheeled/landbasedwheeled.cpp
@@ -1,5 +1,94 @@
 //
 // Created by Sudo on 5/7/2020.
 //
+/**
+* @file landbasedtracked.cpp
+* @authors Group 4
+*
+* Karan Sutradhar (117037272)
+* Sudharsan Balasubramani (116298636)
+* Sai Bhamidipati (117023640)
+* Ashwin Prabhakaran (117030402)
+*
+* @version 1.0
+*
+* @section LICENSE
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation; either version 2 of
+* the License, or (at your option) any later version.
+*
+* @section DESCRIPTION
+*
+*  This is implementation for the LandBasedWheeled class methods.
+*/
 
 #include "landbasedwheeled.h"
+#include <iostream>
+#include "../API/api.h"
+
+//---> Method Definitions <---//
+/**
+ * @brief it is the implementation of fp::LandBasedWheeled::GetDirection() method that gets the direction of the robot in the maze
+ * @param none
+ * @return Returns none
+ */
+char fp::LandBasedWheeled::GetDirection() {
+    std::cout << "LandBasedWheeled::GetDirection is called\n";
+    return this->direction_;
+}
+
+/**
+ * @brief it is the implementation of fp::LandBasedWheeled::MoveForward() method to moves the robot forward in the maze
+ * @param int x
+ * @param int y
+ * @return Returns none
+ */
+void fp::LandBasedWheeled::MoveForward(int x, int y) {
+    std::cout << "LandBasedWheeled::MoveForward is called\n";
+//    fp::API::moveForward();
+}
+
+/**
+ * @brief it is the implementation of fp::LandBasedTracked::TurnLeft() method to rotates the robot 90°counter-clockwise in the maze
+ * @param int x
+ * @param int y
+ * @return Returns none
+ */
+void fp::LandBasedWheeled::TurnLeft(int x, int y) {
+    std::cout << "LandBasedWheeled::TurnLeft is called\n";
+//    fp::API::turnLeft();
+}
+
+/**
+ * @brief it is the implementation of fp::LandBasedTracked::TurnRight() method to rotates the robot 90°clockwise in the maze
+ * @param int x
+ * @param int y
+ * @return Returns none
+ */
+void fp::LandBasedWheeled::TurnRight(int x, int y) {
+    std::cout << "LandBasedWheeled::TurnRight is called\n";
+//    fp::api::turnRight();
+}
+
+//---> Accessors <---//
+/**
+ * @brief it is a getter method for robot wheels number
+ * @param none
+ * @return Returns int wheel_number_
+ */
+int fp::LandBasedWheeled::getWheelNumber() const {
+    return this->wheel_number_;
+}
+
+//---> Mutators <---//
+/**
+ * @brief it is a setter method for the Wheel Number
+ * @param int wheelNumber
+ * @return Returns none
+ */
+void fp::LandBasedWheeled::setWheelNumber(int wheelNumber) {
+    this->wheel_number_ = wheelNumber;
+}
+
--- Final_Project/src/LandBasedWheeled/landbasedwheeled.h
+++ Final_Project/src/LandBasedWheeled/landbasedwheeled.h
@@ -1,14 +1,142 @@
 //
 // Created by Sudo on 5/7/2020.
 //
+/**
+* @file landbasedtracked.h
+* @authors Group 4
+*
+* Karan Sutradhar (117037272)
+* Sudharsan Balasubramani (116298636)
+* Sai Bhamidipati (117023640)
+* Ashwin Prabhakaran (117030402)
+*
+* @version 1.0
+*
+* @section LICENSE
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation; either version 2 of
+* the License, or (at your option) any later version.
+*
+* @section DESCRIPTION
+*
+*  This is the header for the LandBasedWheeled class which is a concrete class and inherits
+ *  the base class LandBasedRobot and declares the necessary attributes
+ *  and method prototypes.
+*/
 
 #ifndef FINAL_PROJECT_LANDBASEDWHEELED_H
 #define FINAL_PROJECT_LANDBASEDWHEELED_H
 
+#include <string>
+#include <iostream>
+#include <memory>
+#include "../LandBasedRobot/landbasedrobot.h"
 
-class LandBasedWheeled {
+namespace fp {
+    class LandBasedWheeled : public LandBasedRobot {
+    protected:
+        //---> Attributes <---//
+        int wheel_number_;                              //--Number of wheels mounted on the robot
+        std::shared_ptr<std::string> wheel_type_;       //--Shared ptr for wheel type attribute
 
-};
+    public:
+        //---> Constructor 01: Default Constructor <---//
+        /**
+         * @brief default constructor of the class LandBasedWheeled
+         */
+        LandBasedWheeled():LandBasedRobot(), wheel_number_{}, wheel_type_{nullptr}{}
 
+        //---> Constructor 02: Constructor with 3 arguments as inputs <---//
+        /**
+         * @brief it is a constructor of the class LandBasedWheeled
+         * @param string name
+         * @param int x
+         * @param int y
+         * @return Returns none
+         */
+
+        LandBasedWheeled(std::string name, int x, int y):
+                LandBasedRobot( name, x, y), wheel_number_{}, wheel_type_{nullptr}{}
+
+        //---> Constructor 03: Constructor with all arguments inputs <---//
+        /**
+         * @brief it is a constructor of the class LandBasedWheeled
+         * @param string name
+         * @param double speed
+         * @param double width
+         * @param double length
+         * @param double height
+         * @param double capacity
+         * @param int x
+         * @param int y
+         * @param char direction
+         * @param int wheel_number
+         * @param shared_ptr wheel_type
+         * @return Returns none
+         */
+
+        LandBasedWheeled(std::string name, double speed, double width, double length, double height, double capacity, int x, int y, char direction, int wheel_number, std::shared_ptr<std::string> wheel_type):
+                LandBasedRobot(name, speed, width, length, height, capacity, x, y, direction), wheel_number_{wheel_number}, wheel_type_{std::move(wheel_type)}{}
+
+        //---> Destructor <---//
+        /**
+         * @brief it is a destructor of the class LandBasedWheeled and deletes the objects created
+         * @param none
+         * @return Returns none
+         */
+
+        ~LandBasedWheeled()= default;
+        //---> Method Prototypes <---//
+        /**
+         * @brief it is a pure virtual method that gets the direction of the robot in the maze
+         * @param none
+         * @return Returns none
+         */
+        char GetDirection() override;       //--Get the direction of the robot in the maze
+
+        /**
+         * @brief it is a pure virtual method that moves the robot forward in the maze
+         * @param int x
+         * @param int y
+         * @return Returns none
+         */
+        void MoveForward(int x, int y) override;     //--Moves the robot forward
+
+        /**
+         * @brief it is a pure virtual method that rotates the robot 90°counter-clockwise in the maze
+         * @param int x
+         * @param int y
+         * @return Returns none
+         */
+        void TurnLeft(int x, int y) override;       //--Rotates the robot 90°counter-clockwise
+
+        /**
+         * @brief it is a pure virtual method that rotates the robot 90°clockwise in the maze
+         * @param int x
+         * @param int y
+         * @return Returns none
+         */
+        void TurnRight(int x, int y) override;      //--Rotates the robot 90°clockwise
+
+        //---> Accessors <---//
+        /**
+         * @brief it is a getter method for robot wheels number
+         * @param none
+         * @return Returns int wheel_number_
+         */
+        int getWheelNumber() const;
+
+        //---> Mutators <---//
+        /**
+         * @brief it is a setter method for the Wheel Number
+         * @param int wheelNumber
+         * @return Returns none
+         */
+        void setWheelNumber(int wheelNumber);
+
+    };//--Class LandBasedWheel
+}//--namespace fp
 
 #endif //FINAL_PROJECT_LANDBASEDWHEELED_H
