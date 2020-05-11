4aaaa9a919ef70a7f342668d71adc1075bb7efd1
Sudharsan <sudharsansci@gmail.com>
2020-05-11 06:28:10 -0400

    Add features to algorithm.cpp and its corresponding prototypes.
    Features:
        -> Create a maze solve function fp::Algorithm::Solve()
        -> Create a robot navigator method fp::Algorithm::Navigate()
        -> Add a method fp::Algorithm::ClearStack() to clear stack_ array
        -> Add duplicate methods for maintaining a local and global history
            -> fp::Algorithm::IsExplored()
        -> Integration with the API class
    
    Updates:
        -> Completed DFS Algorithm
        -> Added fp::Algorithm::Solve() method to solve the Maze on the go by learning the
        maze walls
        -> Added fp::Algorithm::Navigate() method to move the robot in the maze
        -> Add comment and documentation
    Signed-off-by: Sudharsan <sudharsansci@gmail.com>

--- Final_Project/src/Algorithm/algorithm.cpp
+++ Final_Project/src/Algorithm/algorithm.cpp
@@ -18,87 +18,362 @@
 *
 * @section DESCRIPTION
 *
-*  This is the header for the class Algorithm, implementing the Depth First Search algorithm
-*  in this class and navigating the robot through the maze.
+*  This is implementation of Maze solver and Depth first search algorithm.
 */
 
 #include "algorithm.h"
+#include "../API/api.h"
+#include "../Maze/maze.h"
 
-
-bool fp::Algorithm ::IsVisited(std::array<int, 2> cur_node){
-    return visited_node_.at(cur_node.at(0)).at(cur_node.at(1));
+/**
+ * @brief clear the stack
+ * @param none
+ * @return none
+ */
+void fp::Algorithm::ClearStack() {
+    while(!this->stack_.empty()) this->stack_.pop();
 }
 
+/**
+ * @brief takes a node as input and return true if the node visited or else false
+ * @param std::array<int, 2> cur_node
+ * @return Returns int
+ */
+bool fp::Algorithm ::IsVisited(std::array<int, 2> cur_node){
+    return this->visited_node_.at(cur_node.at(0)).at(cur_node.at(1));
+}
+
+/**
+ * @brief takes a node as input and return true if the node explored or else false
+ * @param std::array<int, 2> cur_node
+ * @return Returns int
+ */
+bool fp::Algorithm ::IsExplored(std::array<int, 2> cur_node){
+    return this->explored_node_.at(cur_node.at(0)).at(cur_node.at(1));
+}
+
+/**
+ * @brief finds the neighbouring nodes of a given input node and add the found neighbours to the stack
+ * @param std::array<int, 2> cur_node
+ * @return none
+ */
 void fp::Algorithm::FindNeighbours(std::array<int, 2> cur_node) {
-
     std::array<int, 2> node_N{}, node_W{}, node_S{}, node_E{};
-
-    /* ---> Neighbour in North <--- */
-    node_N[0] = cur_node[0] - 1;
-    node_N[1] = cur_node[1];
-    if (node_N[0] >= 0)
-        if(!IsVisited(node_N)) {
-            stack_.push(node_N);
-            node_arr_[node_N[0]][node_N[1]].parent_node_ = cur_node;
-            if (node_N == goal_) return;
-        }
-
-    /* ---> Neighbour in West <--- */
-    node_W[0] = cur_node[0];
-    node_W[1] = cur_node[1] - 1;
-    if (node_W[1] >= 0)
-        if(!IsVisited(node_W)) {
-            stack_.push(node_W);
-            node_arr_[node_W[0]][node_W[1]].parent_node_ = cur_node;
-            if (node_N == goal_) return;
-        }
 
     /* ---> Neighbour in South <--- */
     node_S[0] = cur_node[0] + 1;
     node_S[1] = cur_node[1];
     if (node_S[0] <= 15)
-        if(!IsVisited(node_S)) {
-            stack_.push(node_S);
-            node_arr_[node_S[0]][node_S[1]].parent_node_ = cur_node;
-            if (node_N == goal_) return;
+        if(!IsExplored(node_S) && !this->maze_info.South_[cur_node[0]][cur_node[1]]) {
+            this->stack_.push(node_S);
+            if(!IsVisited(node_S)) this->node_master_[node_S[0]][node_S[1]].parent_node_ = cur_node;
+            this->node_info[node_S[0]][node_S[1]].parent_node_ = cur_node;
+
+            if (node_S == this->goal1_) return;
+            else if (node_S == this->goal2_) return;
+            else if (node_S == this->goal3_) return;
+            else if (node_S == this->goal4_) return;
         }
 
     /* ---> Neighbour in East <--- */
     node_E[0] = cur_node[0];
     node_E[1] = cur_node[1] + 1;
     if (node_E[1] <= 15)
-        if(!IsVisited(node_E)) {
-            stack_.push(node_E);
-            node_arr_[node_E[0]][node_E[1]].parent_node_ = cur_node;
-            if (node_N == goal_) return;
+        if(!IsExplored(node_E) && !this->maze_info.East_[cur_node[0]][cur_node[1]]) {
+            this->stack_.push(node_E);
+            if(!IsVisited(node_E)) this->node_master_[node_E[0]][node_E[1]].parent_node_ = cur_node;
+            this->node_info[node_E[0]][node_E[1]].parent_node_ = cur_node;
+
+            if (node_E == this->goal1_) return;
+            else if (node_E == this->goal2_) return;
+            else if (node_E == this->goal3_) return;
+            else if (node_E == this->goal4_) return;
+        }
+
+    /* ---> Neighbour in North <--- */
+    node_N[0] = cur_node[0] - 1;
+    node_N[1] = cur_node[1];
+    if (node_N[0] >= 0)
+        if(!IsExplored(node_N) && !this->maze_info.North_[cur_node[0]][cur_node[1]]) {
+            this->stack_.push(node_N);
+            if(!IsVisited(node_N)) this->node_master_[node_N[0]][node_N[1]].parent_node_ = cur_node;
+            this->node_info[node_N[0]][node_N[1]].parent_node_ = cur_node;
+
+            if (node_N == this->goal1_) return;
+            else if (node_N == this->goal2_) return;
+            else if (node_N == this->goal3_) return;
+            else if (node_N == this->goal4_) return;
+        }
+
+    /* ---> Neighbour in West <--- */
+    node_W[0] = cur_node[0];
+    node_W[1] = cur_node[1] - 1;
+    if (node_W[1] >= 0)
+        if(!IsExplored(node_W) && !this->maze_info.West_[cur_node[0]][cur_node[1]]) {
+            this->stack_.push(node_W);
+            if(!IsVisited(node_W)) this->node_master_[node_W[0]][node_W[1]].parent_node_ = cur_node;
+            this->node_info[node_W[0]][node_W[1]].parent_node_ = cur_node;
+
+            if (node_W == this->goal1_) return;
+            else if (node_W == this->goal2_) return;
+            else if (node_W == this->goal3_) return;
+            else if (node_W == this->goal4_) return;
         }
 }
 
-void fp::Algorithm::SearchPath(std::array<int, 2> start, std::array<int, 2> goal) {
+/**
+ * @brief takes start and goal nodes and implements Depth First Search algorithm
+ * @param std::array<int, 2> start
+ * @param std::array<int, 2> goal
+ * @return none
+ */
+bool fp::Algorithm::DFSAlgorithm(std::array<int, 2> start) {
+    //---> Reset Stack and exploration History <---//
+    this->ClearStack();
+    this->explored_node_={};
+
     /* ---> Step 01: Add start to node <--- */
-    stack_.push(start);
-    node_arr_[start[0]][start[1]].parent_node_ = start;
-    goal_ = goal;
-    /* ---> Step 03: loop until stack_ blows <--- */
-    while(!stack_.empty()) {
+    this->stack_.push(start);
+    /* ---> Step 02: Initialize and assign parent node to start node <--- */
+    this->node_info = std::array<std::array<Node, 16>, 16>();
+    this->node_info[start[0]][start[1]].parent_node_ = start;
+    /* ---> Step 03: loop until stack_ exhausts <--- */
+    while(!this->stack_.empty()) {
         /* ---> Step 04: Set Current Node <--- */
-        current_node_ = stack_.top();                                                                   //--> Set the top last-in node as current node
-        stack_.pop();                                                                                   //--> Pop/remove the last element from the stack
+        this->current_node_ = stack_.top();                                                                   //--> Set the top last-in node as current node
+        this->stack_.pop();                                                                                   //--> Pop/remove the last element from the stack
         /* ---> Step 05: check IsGoal <--- */
-        if (current_node_ == goal) break;
-            /* ---> Step 06: Check Visited <--- */
-        else if (!IsVisited(current_node_)) FindNeighbours(current_node_);
+        if (this->current_node_ == this->goal1_) {
+            this->end_goal_ = this->goal1_;
+            return true;
+        }
+        else if (this->current_node_ == this->goal2_) {
+            this->end_goal_ = this->goal2_;
+            return true;
+        }
+        else if (this->current_node_ == this->goal3_) {
+            this->end_goal_ = this->goal3_;
+            return true;
+        }
+        else if (this->current_node_ == this->goal4_) {
+            this->end_goal_ = this->goal4_;
+            return true;
+        }
+        /* ---> Step 06: Check Visited <--- */
+        else if (!IsVisited(this->current_node_) || !IsExplored(this->current_node_))
+            FindNeighbours(this->current_node_);
         /* ---> Step 07: Mark visited <--- */
-        visited_node_[current_node_[0]][current_node_[1]] = true;
+        this->visited_node_[current_node_[0]][current_node_[1]] = true;
+        this->explored_node_[current_node_[0]][current_node_[1]] = true;
+    }
+    return false;
+}
+
+/**
+ * @brief Back track to the start node from a given node
+ * @param std::array<int, 2> start
+ * @return Returns std::stack<std::array<int, 2>>
+ */
+std::stack<std::array<int, 2>> fp::Algorithm::BackTrack(std::array<int, 2> current_node, std::array<std::array<Node, 16>, 16>& node) {
+    std::array<int, 2> parent_node  = node[current_node[0]][current_node[1]].parent_node_;
+    this->path_stack_.push(current_node);
+    while (!(current_node == parent_node)) {
+        current_node = parent_node;
+        parent_node = node[current_node[0]][current_node[1]].parent_node_;
+        this->path_stack_.push(current_node);
+    }
+    return this->path_stack_;
+}
+
+/**
+ * @brief Maze solver algorithm using given search algorithm
+ * @param std::array<int, 2> start
+ * @return Returns std::stack<std::array<int, 2>>
+ */
+void fp::Algorithm::Solve(const std::shared_ptr<fp::LandBasedRobot>& robot) {
+    bool path = false;
+    this->robot = robot;
+    char curr_direction{};
+    std::array<int,2> curr_node{};
+    std::array<int,2> next_node{};
+    std::stack<std::array<int, 2>> local_path;
+
+    curr_direction = robot->get_direction_();
+    curr_node = {robot->get_x_(), robot->get_y_()};
+    this->node_master_[curr_node[0]][curr_node[1]].parent_node_ = curr_node;
+    this->node_master_[curr_node[0]][curr_node[1]].direction_ = curr_direction;
+
+    while(true){
+        //---> Step 01: Clear all tile color <---//
+        fp::API::clearAllColor();
+        //---> Step 02: Get robot current cell info <---//
+        curr_direction = robot->get_direction_();
+        curr_node = {robot->get_x_(), robot->get_y_()};
+        //---> Step 03: Check for goal <---//
+        if (curr_node == this->goal1_) return;
+        else if (curr_node == this->goal2_) return;
+        else if (curr_node == this->goal3_) return;
+        else if (curr_node == this->goal4_) return;
+        //---> Step 04: Read walls around the robot <---//
+        this->maze_info.ReadMaze(curr_node, curr_direction);
+        //---> Step 05: Generate Path using DFS Algorithm <---//
+        path = this->DFSAlgorithm(curr_node);
+        if(!path) return;
+        //---> Step 06: BackTrack the current path <---//
+        local_path = this->BackTrack(this->end_goal_, this->node_info);
+        //---> Step 07: Navigate to next node <---//
+        this->Navigate(local_path);
+    }
+    std::cout<<"Path Doesn't Exist";
+}
+
+/**
+ * @brief Maze solver algorithm using given search algorithm
+ * @param std::array<int, 2> start
+ * @return Returns std::stack<std::array<int, 2>>
+ */
+void fp::Algorithm::Navigate(std::stack<std::array<int, 2>>& local_path) {
+    int x{},y{};
+    char curr_direction{};
+    char direction_togo{};
+    std::array<int, 2> node_curr{}, node_next{};
+
+    //---> Step 01: Extract the current Node and Next Node <---//
+    node_curr = local_path.top();
+    local_path.pop();
+    node_next = local_path.top();
+    local_path.pop();
+
+    //---> Step 02: Compute the togo direction <---//
+    x = node_next[0] - node_curr[0];
+    y = node_next[1] - node_curr[1];
+    curr_direction = this->robot->get_direction_();
+
+    if(x==-1 && y == 0) direction_togo = 'N';
+    else if(x == 1 && y == 0) direction_togo = 'S';
+    else if(x == 0 && y ==-1) direction_togo = 'W';
+    else if(x == 0 && y == 1) direction_togo = 'E';
+
+    //---> Step 03: Navigate the Robot and update new location and direction info <---//
+    if(curr_direction =='N'){
+        if(direction_togo == 'N') {
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+        else if(direction_togo == 'S') {
+            fp::API::turnLeft();
+            fp::API::turnLeft();
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+        else if(direction_togo == 'E') {
+            fp::API::turnRight();
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+        else if(direction_togo == 'W') {
+            fp::API::turnLeft();
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+    }
+    else if(curr_direction =='S') {
+        if(direction_togo == 'N') {
+            fp::API::turnLeft();
+            fp::API::turnLeft();
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+        else if(direction_togo == 'S') {
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+        else if(direction_togo == 'E') {
+            fp::API::turnLeft();
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+        else if(direction_togo == 'W') {
+            fp::API::turnRight();
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+    }
+    else if(curr_direction =='E') {
+        if(direction_togo == 'N') {
+            fp::API::turnLeft();
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+        else if(direction_togo == 'S') {
+            fp::API::turnRight();
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+        else if(direction_togo == 'E') {
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+        else if(direction_togo == 'W') {
+            fp::API::turnLeft();
+            fp::API::turnLeft();
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+    }
+    else if(curr_direction =='W') {
+        if(direction_togo == 'N') {
+            fp::API::turnRight();
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+        else if(direction_togo == 'S') {
+            fp::API::turnLeft();
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+        else if(direction_togo == 'E') {
+            fp::API::turnLeft();
+            fp::API::turnLeft();
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
+        else if(direction_togo == 'W') {
+            fp::API::moveForward();
+            this->robot->set_x_(node_next[0]);
+            this->robot->set_y_(node_next[1]);
+            this->robot->set_direction_(direction_togo);
+        }
     }
 }
 
-std::stack<std::array<int, 2>> fp::Algorithm::BackTrack(std::array<int, 2> current_node) {
-    std::array<int, 2> parent_node  = node_arr_[current_node[0]][current_node[1]].parent_node_;
-    path_stack_.push(current_node);
-    while (!(current_node == parent_node)) {
-        current_node = parent_node;
-        parent_node = node_arr_[current_node[0]][current_node[1]].parent_node_;
-        path_stack_.push(current_node);
-    }
-}
--- Final_Project/src/Algorithm/algorithm.h
+++ Final_Project/src/Algorithm/algorithm.h
@@ -1,7 +1,3 @@
-//
-// Created by Sudo on 5/7/2020.
-//
-
 /**
 * @file algorithm.h
 * @authors Group 4
@@ -22,7 +18,7 @@
 *
 * @section DESCRIPTION
 *
-*  This is the header for the class Algorithm, implementing the Depth First Search algorithm
+*  This is the header for the class Algorithm, implementing the Depth First Search and maze solver algorithm
 *  in this class and navigating the robot through the maze.
 */
 
@@ -33,57 +29,123 @@
 #include <string>
 #include<iostream>
 #include "../API/api.h"
+#include "../LandBasedRobot/landbasedrobot.h"
+#include "../Maze/maze.h"
 #include <map>
 #include <stack>
 #include <array>
 #include <vector>
 #include <utility>
+#include <memory>
 
 
 namespace fp{
     struct Node {
+        //---> Attributes <---//
         int x_, y_, dist_;
         char direction_;
         std::array<int, 2> parent_node_;
+
+        //---> Constructor 01: Default constructor <---//
         Node(): x_{},y_{}, dist_{}, direction_{}, parent_node_{}{}
-        Node(int x, int y, char direction = (char) "") : x_(x), y_(y), direction_(direction), dist_{}, parent_node_{} {}
+
+        //---> Constructor 02: Constructor with 3 arguments as input <---//
+        Node(int x, int y) :
+        x_(x), y_(y), direction_{}, dist_{}, parent_node_{} {}
+
+        //---> Destructors <---//
         ~Node()= default;
     };
 
     class Algorithm {
     public:
+        bool path_found_;
+        fp::Maze maze_info;
         int height_, width_;
         char current_direction_;
-        std::array<int, 2> goal_;
+        std::array<int, 2> goal1_, goal2_, goal3_, goal4_, end_goal_;
         std::array<int, 2> previous_node_;
         std::array<int, 2> current_node_;
         std::stack<std::array <int,2> > stack_;
+        std::shared_ptr<fp::LandBasedRobot> robot;
         std::stack<std::array<int, 2>> path_stack_;
-        std::array<std::array<Node, 16>, 16> node_arr_;
+        std::array<std::array<Node, 16>, 16> node_info;
+        std::array<std::array<Node, 16>, 16> node_master_;
         std::array<std::array<bool, 16>, 16> visited_node_;
+        std::array<std::array<bool, 16>, 16> explored_node_;
+
 
     public:
         //---> Constructor 01: Default Constructor <---//
-        Algorithm(): height_{16}, width_{16}, current_direction_{'N'}, goal_{}, previous_node_{}, current_node_{}, stack_{}, path_stack_{}, node_arr_{}, visited_node_{} {
+        Algorithm(): path_found_{false}, height_{16}, width_{16}, current_direction_{'N'}, goal1_{8,7},
+                     goal2_{8,8}, goal3_{7,8}, goal4_{7,7}, end_goal_{},previous_node_{}, current_node_{}, stack_{},
+                     path_stack_{}, node_master_{}, visited_node_{}, explored_node_{} {
             for(int i = 0; i < height_ ; ++i)
                 for (int j = 0; j < width_ ; ++j) {
-                    node_arr_[i][j] = fp::Node{i, j};
-                    visited_node_[i][j] = false;
+                    this->node_master_[i][j] = fp::Node{i, j};
+                    this->visited_node_[i][j] = false;
                 }
         }
         //---> Destructor <---//
         ~Algorithm()= default;
 
         //---> method prototypes <---//
-
+        /**
+         * @brief takes a node as input and return true if the node visited or else false
+         * @param std::array<int, 2> cur_node
+         * @return Returns bool
+         */
         bool IsVisited(std::array<int, 2> cur_node);
 
+        /**
+         * @brief takes a node as input and return true if the node visited or else false
+         * @param std::array<int, 2> cur_node
+         * @return Returns bool
+         */
+        bool IsExplored(std::array<int, 2> cur_node);
+
+        /**
+         * @brief finds the neighbouring nodes of a given input node and add the found neighbours to the stack
+         * @param std::array<int, 2> cur_node
+         * @return none
+         */
         void FindNeighbours(std::array<int, 2> cur_node);
 
-        void SearchPath(std::array<int, 2> start, std::array<int, 2> goal);
+        /**
+         * @brief takes start and goal nodes and implements Depth First Search algorithm
+         * @param std::array<int, 2> start
+         * @param std::array<int, 2> goal
+         * @return none
+         */
+        bool DFSAlgorithm(std::array<int, 2> start);
 
-        std::stack<std::array<int, 2>> BackTrack(std::array<int, 2> current_node);
+        /**
+         * @brief Back track to the start node from a given node
+         * @param std::array<int, 2> start
+         * @return Returns std::stack<std::array<int, 2>>
+         */
+        std::stack<std::array<int, 2>> BackTrack(std::array<int, 2> current_node, std::array<std::array<Node, 16>, 16>& node);
 
+        /**
+         * @brief Maze solver algorithm using given search algorithm
+         * @param std::array<int, 2> start
+         * @return Returns std::stack<std::array<int, 2>>
+         */
+        void Solve(const std::shared_ptr<fp::LandBasedRobot>& robot);
+
+        /**
+         * @brief Maze solver algorithm using given search algorithm
+         * @param std::array<int, 2> start
+         * @return Returns std::stack<std::array<int, 2>>
+         */
+        void Navigate(std::stack<std::array<int, 2>>& path);
+
+        /**
+         * @brief clear the stack
+         * @param none
+         * @return none
+         */
+        void ClearStack();
     };
 }
 
